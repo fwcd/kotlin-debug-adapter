6dde051f7ca8ac3c90b09ec896488b9d89b96d8c
fwcd <fwcdmail@gmail.com>
2018-08-20 12:27:51 +0200

    Implemented LineNumberConverter

--- src/main/kotlin/com/fwcd/ktda/core/LineNumberConverter.kt
+++ src/main/kotlin/com/fwcd/ktda/core/LineNumberConverter.kt
@@ -1,5 +1,18 @@
 package com.fwcd.ktda.core
 
-class LineNumberConverter {
+/**
+ * Converts between external and internal line numbering.
+ * For example:
+ *
+ * An "external" (debug adapter) line number could be zero-indexed and
+ * an "internal" (core) line number would be one-indexed.
+ *
+ * In this case, externalLineOffset would be -1.
+ */
+class LineNumberConverter(
+	private val externalLineOffset: Int // Internal line + externalLineOffset = External line
+) {
+	fun toInternalLine(lineNumber: Int) = lineNumber - externalLineOffset
 	
+	fun toExternalLine(lineNumber: Int) = lineNumber + externalLineOffset
 }
