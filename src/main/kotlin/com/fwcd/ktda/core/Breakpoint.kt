a58616b92fd72278f25183b75e0134650947050d
fwcd <fwcdmail@gmail.com>
2018-08-20 11:33:38 +0200

    ObservableList

--- src/main/kotlin/com/fwcd/ktda/core/Breakpoint.kt
+++ src/main/kotlin/com/fwcd/ktda/core/Breakpoint.kt
@@ -1,8 +1,5 @@
 package com.fwcd.ktda.core
 
-typealias DAPSourceBreakpoint = org.eclipse.lsp4j.debug.SourceBreakpoint
-typealias DAPBreakpoint = org.eclipse.lsp4j.debug.Breakpoint
-
 class Breakpoint {
 	
 }
---
+++ src/main/kotlin/com/fwcd/ktda/util/ObservableList.kt
@@ -0,0 +1,35 @@
+package com.fwcd.ktda.util
+
+class ObservableList<T>(
+	private val entries: MutableList<T> = mutableListOf()
+) {
+	private val listeners = ListenerList<List<T>>()
+	
+	val size: Int
+		get() = entires.size
+	
+	fun add(element: T) {
+		entries.add(element)
+		fire()
+	}
+	
+	fun remove(element: T) {
+		entries.remove(element)
+		fire()
+	}
+	
+	operator fun get(index: Int) = entries[index]
+	
+	operator fun set(index: Int, value: T) {
+		entries[index] = value
+		fire()
+	}
+	
+	fun asSequence(): Sequence<T> = entries
+	
+	fun listen(listener: (List<T>) -> Unit) = listeners.add(listener)
+	
+	fun unlisten(listener: (List<T>) -> Unit) = listeners.remove(listener)
+	
+	private fun fire() = listeners.fire(value)
+}
