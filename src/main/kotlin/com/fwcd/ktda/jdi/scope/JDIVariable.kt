a66dd23c55c8f7b0b34ca8c526b59fced9fa304b
fwcd <fwcdmail@gmail.com>
2018-08-21 15:23:10 +0200

    Fixed #4 (JDI exception when inspecting null variable)

--- src/main/kotlin/com/fwcd/ktda/jdi/scope/JDIVariable.kt
+++ src/main/kotlin/com/fwcd/ktda/jdi/scope/JDIVariable.kt
@@ -1,21 +1,22 @@
 package com.fwcd.ktda.jdi.scope
 
 import com.fwcd.ktda.core.scope.VariableTreeNode
-import com.fwcd.ktda.LOG
 import com.sun.jdi.ReferenceType
 import com.sun.jdi.LocalVariable
 import com.sun.jdi.ArrayType
 import com.sun.jdi.ArrayReference
 import com.sun.jdi.ObjectReference
 import com.sun.jdi.Value
+import com.sun.jdi.Type
 
 class JDIVariable(
 	override val name: String,
-	jdiValue: Value
+	jdiValue: Value?,
+	jdiType: Type? = null
 ): VariableTreeNode {
-	override val value: String = jdiValue.toString() // TODO: Better string representation
-	override val type: String = jdiValue.type().name()
-	override val childs: List<VariableTreeNode> by lazy { childrenOf(jdiValue) }
+	override val value: String = jdiValue?.toString() ?: "null" // TODO: Better string representation
+	override val type: String = (jdiType?.name() ?: jdiValue?.type()?.name()) ?: "Unknown type"
+	override val childs: List<VariableTreeNode> by lazy { jdiValue?.let(::childrenOf) ?: emptyList() }
 	
 	private fun childrenOf(jdiValue: Value): List<VariableTreeNode> {
 		val jdiType = jdiValue.type()
@@ -33,5 +34,5 @@ class JDIVariable(
 		.mapIndexed { i, it -> JDIVariable(i.toString(), it) }
 		
 	private fun fieldsOf(jdiValue: ObjectReference, jdiType: ReferenceType) = jdiType.allFields()
-		.map { JDIVariable(it.name(), jdiValue.getValue(it)) }
+		.map { JDIVariable(it.name(), jdiValue.getValue(it), jdiType) }
 }
