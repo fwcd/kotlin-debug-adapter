cf79619f3b490f366aacbd669bc26fdfd38d4870
fwcd <fwcdmail@gmail.com>
2020-06-06 21:41:38 +0200

    Build project before launching in test fixture

--- adapter/src/test/kotlin/org/javacs/ktda/DebugAdapterTestFixture.kt
+++ adapter/src/test/kotlin/org/javacs/ktda/DebugAdapterTestFixture.kt
@@ -10,30 +10,45 @@ import org.eclipse.lsp4j.debug.services.IDebugProtocolClient
 import org.javacs.ktda.adapter.KotlinDebugAdapter
 import org.javacs.ktda.jdi.launch.JDILauncher
 import org.junit.After
+import org.junit.Assert.assertThat
+import org.junit.Before
+import org.hamcrest.Matchers.equalTo
 
 abstract class DebugAdapterTestFixture(
     relativeWorkspaceRoot: String,
     private val mainClass: String
 ) : IDebugProtocolClient {
     val absoluteWorkspaceRoot: Path = Paths.get(DebugAdapterTestFixture::class.java.getResource("/Anchor.txt").toURI()).parent.resolve(relativeWorkspaceRoot)
-    val debugAdapter: KotlinDebugAdapter = JDILauncher()
-        .let(::KotlinDebugAdapter)
-        .also {
-            it.connect(this)
-            val configDone = it.configurationDone(ConfigurationDoneArguments())
-            it.initialize(InitializeRequestArguments().apply {
-                adapterID = "test-debug-adapter"
-                linesStartAt1 = true
-                columnsStartAt1 = true
-            }).join()
-            // Slightly hacky workaround to ensure someone is
-            // waiting on the ConfigurationDoneResponse. See
-            // KotlinDebugAdapter.kt:performInitialization for
-            // details.
-            Thread {
-                configDone.join()
-            }.start()
-        }
+    lateinit var debugAdapter: KotlinDebugAdapter
+    
+    @Before fun startDebugAdapter() {
+        // Build the project first
+        val process = ProcessBuilder("./gradlew", "assemble")
+            .directory(absoluteWorkspaceRoot.toFile())
+            .inheritIO()
+            .start()
+        process.waitFor()
+        assertThat(process.exitValue(), equalTo(0))
+        
+        debugAdapter = JDILauncher()
+            .let(::KotlinDebugAdapter)
+            .also {
+                it.connect(this)
+                val configDone = it.configurationDone(ConfigurationDoneArguments())
+                it.initialize(InitializeRequestArguments().apply {
+                    adapterID = "test-debug-adapter"
+                    linesStartAt1 = true
+                    columnsStartAt1 = true
+                }).join()
+                // Slightly hacky workaround to ensure someone is
+                // waiting on the ConfigurationDoneResponse. See
+                // KotlinDebugAdapter.kt:performInitialization for
+                // details.
+                Thread {
+                    configDone.join()
+                }.start()
+            }
+    }
     
     fun launch() {
         println("Launching...")
