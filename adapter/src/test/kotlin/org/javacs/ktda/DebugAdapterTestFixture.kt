b85bc6c525b050b72b7b7238c830f91fdb159104
fwcd <fwcdmail@gmail.com>
2020-06-06 21:12:25 +0200

    Automate initialization sequence in test fixture

--- adapter/src/test/kotlin/org/javacs/ktda/DebugAdapterTestFixture.kt
+++ adapter/src/test/kotlin/org/javacs/ktda/DebugAdapterTestFixture.kt
@@ -2,6 +2,8 @@ package org.javacs.ktda
 
 import java.nio.file.Path
 import java.nio.file.Paths
+import org.eclipse.lsp4j.debug.ConfigurationDoneArguments
+import org.eclipse.lsp4j.debug.InitializeRequestArguments
 import org.eclipse.lsp4j.debug.DisconnectArguments
 import org.eclipse.lsp4j.debug.OutputEventArguments
 import org.eclipse.lsp4j.debug.services.IDebugProtocolClient
@@ -9,17 +11,37 @@ import org.javacs.ktda.adapter.KotlinDebugAdapter
 import org.javacs.ktda.jdi.launch.JDILauncher
 import org.junit.After
 
-abstract class DebugAdapterTestFixture(relativeWorkspaceRoot: String, mainClass: String) : IDebugProtocolClient {
+abstract class DebugAdapterTestFixture(
+    relativeWorkspaceRoot: String,
+    private val mainClass: String
+) : IDebugProtocolClient {
     val absoluteWorkspaceRoot: Path = Paths.get(DebugAdapterTestFixture::class.java.getResource("/").toURI()).resolve(relativeWorkspaceRoot)
     val debugAdapter: KotlinDebugAdapter = JDILauncher()
         .let(::KotlinDebugAdapter)
-        .also { it.connect(this) }
+        .also {
+            it.connect(this)
+            val configDone = it.configurationDone(ConfigurationDoneArguments())
+            it.initialize(InitializeRequestArguments().apply {
+                adapterID = "test-debug-adapter"
+                linesStartAt1 = true
+                columnsStartAt1 = true
+            }).join()
+            // Slightly hacky workaround to ensure someone is
+            // waiting on the ConfigurationDoneResponse. See
+            // KotlinDebugAdapter.kt:performInitialization for
+            // details.
+            Thread {
+                configDone.join()
+            }.start()
+        }
     
     fun launch() {
+        println("Launching...")
         debugAdapter.launch(mapOf(
             "projectRoot" to absoluteWorkspaceRoot.toString(),
-            "mainClass" to "sample.workspace.AppKt"
-        ))
+            "mainClass" to mainClass
+        )).join()
+        println("Launched")
     }
 
     @After fun closeDebugAdapter() {
