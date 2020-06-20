448cc031f818a977ab4ec61026e1d02e37eec6c3
fwcd <fwcdmail@gmail.com>
2020-06-20 23:44:35 +0200

    Make Source a data class

--- adapter/src/main/kotlin/org/javacs/ktda/core/Source.kt
+++ adapter/src/main/kotlin/org/javacs/ktda/core/Source.kt
@@ -3,11 +3,7 @@ package org.javacs.ktda.core
 import java.nio.file.Path
 
 /** A source unit descriptor (usually a file) */
-class Source(
+data class Source(
 	val name: String,
 	val filePath: Path
-) {
-	override fun equals(other: Any?): Boolean {
-		return (other is Source) && (name == other.name) && (filePath == other.filePath)
-	}
-}
+)
